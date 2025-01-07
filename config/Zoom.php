47d5fbc6273284c1a2a4242683953ad48ffcd369
naveenkumar <naveenkumar.dps@gmail.com>
2025-01-07 11:52:46 +0530

    Config file added

---
+++ config/Zoom.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'client_id' => env('ZOOM_CLIENT_ID'),
+    'client_secret' => env('ZOOM_CLIENT_SECRET'),
+    'redirect_uri' => env('ZOOM_REDIRECT_URI'),
+    'base_url' => env('ZOOM_BASE_URL'),
+    'account_id' => env('ZOOM_ACCOUNT_ID'),
+];
